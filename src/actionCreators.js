38f1e640a8534ebdb466bd56329e37d40b30f41f
neumann11 <pavlo.vlasyuk@gmail.com>
2018-05-08 14:15:49 +0200

    Connect backend server with front end React-Redux Todo App, use Redux-Thunk to allow actionCreators return async functions that return actions after AJAX call complete

--- src/actionCreators.js
+++ src/actionCreators.js
@@ -1,18 +1,63 @@
 export const ADD_TODO = "ADD_TODO";
 export const REMOVE_TODO = "REMOVE_TODO";
+export const GET_TODOS = "GET_TODOS"; //Retreive all Todos
 
 //action creators functions:
 
-export function addTodo(task){
+function handleTodos(data){
   return{
-    type: ADD_TODO,
-    task
+    type: GET_TODOS,
+    data
   };
 }
 
-export function removeTodo(id){
+function handleAdd(todo){
+  return{
+    type: ADD_TODO,
+    todo
+  };
+}
+
+function handleRemove(id){
   return{
     type: REMOVE_TODO,
     id
   };
+}
+
+//action creators that return functions with Redux-Thunk:
+
+export function getTodos(){
+  return dispatch => {
+    return fetch("http://localhost:3001/api/todos")
+      .then(res => res.json())
+      .then(data => dispatch(handleTodos(data))) //disp. action creator
+      .catch(err => console.log("SOMETHING WENT WRONG", err));
+  };
+}
+
+export function addTodo(task){
+  return dispatch => {
+    return fetch("http://localhost:3001/api/todos", {
+      method: "POST",
+      headers: new Headers({
+        "Content-Type": "application/json"
+      }),
+      body: JSON.stringify({ task })
+    })
+    .then(res => res.json())
+    .then(data => dispatch(handleAdd(data)))
+    .catch(err => console.log("SOMETHING WENT WRONG", err))
+  };
+}
+
+export function removeTodo(id){
+  return dispatch => {
+    return fetch(`http://localhost:3001/api/todos/${id}`, {
+      method: "DELETE"
+    })
+    .then(res => res.json())
+    .then(data => dispatch(handleRemove(id)))
+    .catch(err => console.log("SOMETHING WENT WRONG", err))
+  }
 }
--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore} from 'redux'; // Redux.createStore
+import { createStore, applyMiddleware, compose } from 'redux'; // Redux.createStore
 import rootReducer from "./rootReducer";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-
+import thunk from "redux-thunk" //helps make redux actions asynchronous for AJAX calls
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose( //compose all middleware together into one function
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 ReactDOM.render(
